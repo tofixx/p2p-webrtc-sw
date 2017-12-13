0d854a7b171be70a8ce77074967eaa6c9b793be7
Tobias Wollowski <tobias@jbbr.net>
2017-12-13 17:35:22 +0100

    small corrections

--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
+
 /**
  * copied from https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker (2017-12-13)
  */
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.open('files').then(function (cache) {
+            console.log('[SW] handle request event', event.request);            
             return cache.match(event.request).then(function (response) {
                 if (response) {
-                    console.log("serve data fromcache...", response);
-                } else {
-                    console.log("serve data from network...")
-                }
+                    console.log("[SW] serve data fromcache...", response);
+                } 
                 return response || fetch(event.request).then(function (response) {
+                    console.log("[SW] load data from network...")                    
                     //cache.put(event.request, response.clone());
                     return response;
                 });
