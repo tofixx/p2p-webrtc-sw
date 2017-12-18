3e85aa9da92bff46f73c55466cd35b6f7c0d0987
Tobias Wollowski <tobias@jbbr.net>
2017-12-18 11:12:38 +0100

    added auto response with file based on request

--- sw.js
+++ sw.js
@@ -3,19 +3,39 @@
  * copied from https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker (2017-12-13)
  */
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.open('files').then(function (cache) {
-            console.log('[SW] handle request event', event.request);            
-            return cache.match(event.request).then(function (response) {
-                if (response) {
-                    console.log("[SW] serve data fromcache...", response);
-                } 
-                return response || fetch(event.request).then(function (response) {
-                    console.log("[SW] load data from network...")                    
-                    //cache.put(event.request, response.clone());
-                    return response;
+    var path = new URL(event.request.url).pathname;
+    if(path.startsWith('/location')){
+        event.respondWith(
+            caches.open('files').then(function (cache) {
+                console.log('[SW] handle request event', event.request);            
+                return cache.match(event.request).then(function (response) {
+                    if (response) {
+                        console.log("[SW] serve data from cache...", response);
+                    } 
+                    return response || fetch(event.request).then(function (response) {
+                        console.log("[SW] load data from network...")                    
+                        return response;
+                    });
                 });
-            });
-        })
-    );
+            })
+        );
+    }else if(path.startsWith('/cache')){
+        var url = new URL(event.request.url);
+        url.pathname = path.slice(6); // removes "/cache" prefix     
+        event.respondWith(
+            caches.open('cache').then(function (cache) {
+                console.log('[SW-Cache] handle request event', url);            
+                return cache.match(url).then(function (response) {
+                    if (response) {
+                        console.log("[SW-Cache] serve data from cache...", response);
+                    } 
+                    return response || fetch(url).then(function (response) {
+                        console.log("[SW-Cache] load data from network and add data to cache...")                    
+                        cache.put(url, response.clone());
+                        return response;
+                    });
+                });
+            })
+        );
+    }
 });
